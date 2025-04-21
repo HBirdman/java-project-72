082acb1ea9128c81fedd344b6fd3f33b0cf34ed9
HBirdman <hbirdman@mail.ru>
2025-04-21 19:52:02 +0300

    Preparing project for a primary deploy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,14 @@
 plugins {
     id("java")
+    application
+    checkstyle
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
@@ -10,6 +19,15 @@ repositories {
 }
 
 dependencies {
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("io.javalin:javalin:6.6.0")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+
+    testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
